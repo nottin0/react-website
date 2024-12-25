8e5853042c4209cbb0d500e1eaede2f8ba8a396f
tinodev <tinovalentine5@gmail.com>
2024-12-25 18:08:52 -0500

    fixed broken site

--- reactprac/prac/src/supabaseClient.js
+++ reactprac/prac/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dgzqsvnoqjghquprliyj.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://dgvqsvnoqjghquprliyj.supabase.co' 
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnenFzdm5vcWpnaHF1cHJsaXlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxMTU4OTAsImV4cCI6MjA1MDY5MTg5MH0.uef-eeICOGovBeiTbVejl-xLJIYZlAPxwLAztRkVeXs' 
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
