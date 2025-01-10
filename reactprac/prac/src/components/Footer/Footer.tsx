9a323a08cfab48e9698e0da20a48ffc18dc7719c
tinodev <tinovalentine5@gmail.com>
2025-01-10 03:51:29 -0500

    v 0.5 - New blog posts

--- reactprac/prac/src/components/Footer/Footer.tsx
+++ reactprac/prac/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   const year = date.getFullYear();
   return (
     <footer className="text-center py-8 bg-gray-900 mt-auto">
-      <p className="text-gray-400">Version 0.3.5</p>
+      <p className="text-gray-400">Version 0.5</p>
       <p className="text-gray-400">Built with ❤️ by tinodev</p>
       <p className="text-gray-400">Copyright {year} tinodev</p>
     </footer>
